a81f66d311a3a28ba1a61c3d79b36fa4769e6f9a
zhouwenxiaobupt <46025787+zhouwenxiaobupt@users.noreply.github.com>
2018-12-20 21:44:13 +0800

    Add files via upload

---
+++ pages/background-audio/background-audio.js
@@ -0,0 +1,10 @@
+const backgroundAudioManager = wx.getBackgroundAudioManager()
+
+backgroundAudioManager.title = 'time travel'
+backgroundAudioManager.epname = 'time travel'
+backgroundAudioManager.singer = '岸部真明'
+backgroundAudioManager.coverImgUrl = 'https://api.superbed.cn/pic/5c020e6cc4ff9e05824609e4'
+// 设置了 src 之后会自动播放
+backgroundAudioManager.src = ' http://sc1.111ttt.cn/2016/5/12/04/205041229057.mp3'
+Page({
+})
